8aba4b8e1f56015b59b6b93d25b93b8580cf96aa
Susheel Kumar <thotasusheelkumar04@gmail.com>
2025-05-31 15:06:01 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:
    
    And change it as follows:
    
    Update the following images in this component:
    
    Image 1:
             - Original image: /_next/image?url=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1544005313-94ddf0286df2%3Fcrop%3Dentropy%26cs%3Dtinysrgb%26fit%3Dmax%26fm%3Djpg%26ixid%3DM3w3NDE5ODJ8MHwxfHNlYXJjaHwxN3x8cnVzc2lhbiUyMGdpcmx8ZW58MHx8fHwxNzQ4NzAzODkwfDA%26ixlib%3Drb-4.1.0%26q%3D80%26w%3D1080&w=3840&q=75
             - New image URL: https://images.unsplash.com/photo-1597586124394-fbd6ef244026?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxNXx8Z2lybHN8ZW58MHx8fHwxNzQ4NzAzOTMzfDA&ixlib=rb-4.1.0&q=80&w=1080
             - Hint used: "woman portrait"
    
    Replace the image sources with the new URLs. This may involve updating src, srcset, background-image, or other relevant attributes depending on the element type.

--- src/components/sections/gallery-section.tsx
+++ src/components/sections/gallery-section.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const galleryItems = [
   { src: "https://images.unsplash.com/photo-1522335789203-aabd1fc54bc9?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxNXx8bWFrZXVwJTIwa2l0fGVufDB8fHx8MTc0ODcwMzU2MHww&ixlib=rb-4.1.0&q=80&w=1080", alt: "Makeup kit and products", dataAiHint: "makeup kit" },
-  { src: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxN3x8cnVzc2lhbiUyMGdpcmx8ZW58MHx8fHwxNzQ4NzAzODkwfDA&ixlib=rb-4.1.0&q=80&w=1080", alt: "Portrait of a woman", dataAiHint: "woman portrait" },
+  { src: "https://images.unsplash.com/photo-1597586124394-fbd6ef244026?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxNXx8Z2lybHN8ZW58MHx8fHwxNzQ4NzAzOTMzfDA&ixlib=rb-4.1.0&q=80&w=1080", alt: "Smiling woman", dataAiHint: "smiling woman" },
   { src: "https://images.unsplash.com/photo-1596205521983-9c372fb3d4f1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw3fHxtYWtldXB8ZW58MHx8fHwxNzQ4NzAzNTA1fDA&ixlib=rb-4.1.0&q=80&w=1080", alt: "Makeup application", dataAiHint: "makeup" },
 ];
 
